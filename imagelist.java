4e33aafa19bc7870543a44671545b1b3d76fbb43
Siddharrth Priyadharsan <siddumsp1997@gmail.com>
2016-07-30 10:13:45 -0700

    Update imagelist.java

--- imagelist.java
+++ imagelist.java
@@ -1,11 +1,10 @@
 
 import java.util.ArrayList;
 
-
+//Class for image list
 public class imagelist {
-
+       //Static components of the album
 	public static  ArrayList<image> images;
-	
 	public static fileclass ff;
 }
-
+//End of class
