d396fe317f164f31be1af1316c3a2d1d1d84730f
Siddharrth Priyadharsan <siddumsp1997@gmail.com>
2016-07-30 09:57:48 -0700

    Update openphoto.java

--- openphoto.java
+++ openphoto.java
@@ -1,24 +1,25 @@
+// Swing packages for the GUI
 import java.awt.*;
 import java.util.*;
 import java.io.*;
 import java.awt.event.*;
-
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
-
 import java.lang.*;
 
-
+// Class open photo for displaying every photo (Frame)
 public class openphoto extends JFrame {
 	
+	//Components of the frame
 	public image f;
 	private JTextField textField;
 	private ImageIcon image1;
 	private ImageLabel imageLabel;
 	
+	//Class constructor
 	public openphoto(image i)
 	{   
-		super(i.title);
+		super(i.title);   // Frame title
 		getContentPane().setLayout(null);
 		f=i;
 		Font pf2=new Font("Serif",Font.PLAIN,22);
@@ -26,12 +27,12 @@ public class openphoto extends JFrame {
 	    image1 = new ImageIcon(f.location);
 	    imageLabel = new ImageLabel(image1);
 	    imageLabel.setBounds(23,25,1185,878);
-		imageLabel.setVisible(true);
+	    imageLabel.setVisible(true);
 		
 		imageLabel.setToolTipText(f.annotation);
-	//	imageLabel.setSize(859,696);
 		getContentPane().add(imageLabel);
 		
+		// Button to go to main menu
 		JButton btnNewButton = new JButton("<<BACK TO ALBUM");
 		btnNewButton.setBounds(33, 916, 579, 45);
 		btnNewButton.addActionListener(new ActionListener() {
@@ -39,6 +40,7 @@ public class openphoto extends JFrame {
 				
 				  setVisible(false);
 				  album piq=new album();
+				  //Going to previous frame
 		          piq.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		  		  piq.setSize(1000,904);
 		  		  piq.setVisible(true);
@@ -47,16 +49,15 @@ public class openphoto extends JFrame {
 		getContentPane().add(btnNewButton);
 		btnNewButton.setFont(pf2);
 		
-		
-		
+		// Button to delete the correponding picture
 		JButton btnNewButton_1 = new JButton("REMOVE THIS PICTURE FROM ALBUM");
 		btnNewButton_1.setBounds(641, 916, 567, 45);
 		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				setVisible(false);
-				imagelist.images.remove(f);
+				imagelist.images.remove(f); //Removing picture
 				
-				try {
+				try {   //Updating the text file to save changes
 					imagelist.ff.updatefile(imagelist.images);
 				} catch (Exception e1) {
 					// TODO Auto-generated catch block
@@ -64,18 +65,16 @@ public class openphoto extends JFrame {
 				}
 				JOptionPane.showMessageDialog(null,"Photo removed from album successfully !!");
 				
-				album piq=new album();
-		        piq.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		          //Going to main menu		
+			    album piq=new album();
+		            piq.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		  	    piq.setSize(1000,904);
 		  	    piq.setVisible(true);
-				
 				
 			}
 		});
 		getContentPane().add(btnNewButton_1);
 		btnNewButton_1.setFont(pf2);
-		
-		
 		
 	}//end of constructor
 
