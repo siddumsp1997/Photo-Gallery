35f501ed5ec4ca51cdc797cb0bc590e2dd005a10
Siddharrth Priyadharsan <siddumsp1997@gmail.com>
2016-07-30 10:01:00 -0700

    Update mainclass.java

--- mainclass.java
+++ mainclass.java
@@ -1,16 +1,15 @@
-
+//Importing packages for the GUI
 import java.awt.*;
 import java.util.*;
 import java.io.*;
 import java.awt.event.*;
-
 import javax.swing.*;
 
+// Main class
 public class mainclass extends JFrame{
 	
 	public static void main(String args[])
-	{
-		
+	{       // Arraylist for storing images' details
 		imagelist.images=new ArrayList<>();
 		
 		try {
@@ -20,22 +19,19 @@ public class mainclass extends JFrame{
 			e.printStackTrace();
 		} 
 		
+		//Moving to the main menu
 		firstpage fd=new firstpage();
 		fd.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		fd.setSize(540,500);
 		fd.setVisible(true);
 		
 		
-		try {
+		try {   //Closing the text file after the program is closed
 			imagelist.ff.closefile();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	}
-	
-	
-	
+	} //End of main function
 }
-
-//imagelist.ff.updatefile(imagelist.images);
+//End of main class
