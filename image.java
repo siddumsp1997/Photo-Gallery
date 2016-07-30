ad47def78cfce93cffc6c1d67fee10132ceeb933
Siddharrth Priyadharsan <siddumsp1997@gmail.com>
2016-07-30 10:12:31 -0700

    Update image.java

--- image.java
+++ image.java
@@ -2,15 +2,18 @@ import java.util.*;
 import java.io.*;
 import java.lang.*;
 
+// Class for image
 public class image {
-
+        // Components of image
 	public String location;
 	public String title;
 	public String annotation;
 	
-	
+	//Class constructor
 	public image()
 	{
 		location=title=annotation="";
 	}
+	//End of constructor
 }
+//End of class
