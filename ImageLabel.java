515580175ce3a90b8e21cc6ed9046e8027c96c5a
Siddharrth Priyadharsan <siddumsp1997@gmail.com>
2016-07-30 03:50:41 -0700

    Update ImageLabel.java

--- ImageLabel.java
+++ ImageLabel.java
@@ -1,61 +1,50 @@
+// Importing swing packages for GUI
 import java.awt.*;
 import java.util.*;
 import java.io.*;
 import java.awt.event.*;
 import java.awt.image.ImageObserver;
-
 import javax.swing.*;
-
 import java.lang.*;
 
+//Class for Image label
 class ImageLabel extends JLabel {
-
+   
+   //Components of the Image label
    Image image;
    ImageObserver imageObserver;
 
    // constructor with filename    
-
    ImageLabel(String filename) {
 
-      ImageIcon icon = new ImageIcon(filename);
-
+      ImageIcon icon = new ImageIcon(filename);   // File name of the image
       image = icon.getImage();
-
       imageObserver = icon.getImageObserver();
 
    }
-
- 
-
+   
+   // constructor with Image icon
    ImageLabel(ImageIcon icon) {
-   image = icon.getImage();
-
+   
+      image = icon.getImage();     
       imageObserver = icon.getImageObserver();
 
-   }
-
- 
-
-   // overload setIcon method
+   } // overload setIcon method
 
    void setIcon(ImageIcon icon) {
 
       image = icon.getImage();
-
       imageObserver = icon.getImageObserver();
 
    }
-
- 
-
    // overload paint()
 
    public void paint( Graphics g ) {
 
        super.paint( g );
-
        g.drawImage(image,  0 , 0 , getWidth() , getHeight() , imageObserver);
 
    }
 
 }
+//End of class
